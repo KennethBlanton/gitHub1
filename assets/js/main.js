df5fbef1e6941c761e4de7c395586cc30c035255
KennethBlanton <Stella@Kenneths-MacBook-Pro.local>
2017-01-26 13:13:27 -0500

    Only works at fixed screen none responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 console.log("Hello World from main.js!");
+
+// var $kayak = document.querySelector(".kayak-body");
+// var rightPressed = false;
+// var leftPressed = true;
+// function keyDownHandler(e) {
+// 	if(e.keyCode == 39) {
+// 		rightPressed = true;
+// 		moveRight();
+// 	} else if(e.keyCode == 37) {
+// 		leftPressed = true;
+// 		moveLeft();
+// 	}
+// }
+// var moveRight = function() {
+// 	clearInterval(id);
+// 	var id = setInterval(move, 5);
+// 	pos = 0;
+// 	function move() {
+// 		if(pos >= 100) {
+// 			clearInterval(id);
+// 			pos=0;
+// 		}else if(posLeft>= window.innerWidth) {
+// 			posLeft = 0;
+// 			$kayak.style.left = posLeft + "px";
+// 			pos++;
+// 		} else {
+// 			posLeft = posLeft + .5;
+// 			$kayak.style.left = posLeft + "px";
+// 			console.log("moving");
+// 			pos++;
+// 	}
+// }
+// document.addEventListener("keydown", keyDownHandler, false);
+// document.addEventListener("keyup" , keyUpHandler, false);
